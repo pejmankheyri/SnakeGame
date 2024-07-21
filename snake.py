1f11e92dc409dfee5c650aae522018ec9c895581
pejmankheyri <pejmankheyri@gmail.com>
2024-07-22 00:43:09 +0200

    Fix collision detection and reset functionality

--- snake.py
+++ snake.py
@@ -25,6 +25,13 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
